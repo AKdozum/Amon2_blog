78c9c319396f61a3bfc9451a889a4d1bd6e22a7b
AKdozum <uhavetwocows@gmail.com>
2014-02-16 13:45:50 +0900

    Created a DB schema

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -1,4 +1,19 @@
+PRAGMA foreign_keys=ON; #https://www.sqlite.org/foreignkeys.html
 CREATE TABLE IF NOT EXISTS member (
     id           INTEGER NOT NULL PRIMARY KEY,
     name         VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS entry (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    body TEXT not null,
+    created DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS comment (
+    id           INTEGER NOT NULL PRIMARY KEY,
+    name         VARCHAR(255),
+    body         TEXT not null,
+    created      DATETIME,
+    FOREIGN KEY(id) REFERENCES entry(id)
 );
